f566c5bebeb25fc32a3bef1995285fa3e1de8399
hoagduc27 <85150430+hoagduc27@users.noreply.github.com>
2022-11-15 22:00:17 +0700

    them_func_trong_dbconnect
    để dễ query

--- PRO1041_IT17316_N5/src/Ultilities/SQLServerConnect.java
+++ PRO1041_IT17316_N5/src/Ultilities/SQLServerConnect.java
@@ -6,6 +6,8 @@ package Ultilities;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -36,5 +38,45 @@ public class SQLServerConnect {
 
     public static void main(String[] args) {
         System.out.println(getConnection());
+    }
+    
+    private static Connection conn;
+    //2. Thực thi truy vấn Thêm, Sửa , Xoá
+    public static int ExcuteDungna(String sql, Object... args) {
+        PreparedStatement pstm = getStmt(sql, args);
+        try {
+            try {
+                return pstm.executeUpdate();
+            } finally {
+                pstm.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println("Lỗi tại ExcuteDungna");
+            return 0;
+        }
+    }
+
+    //3. Trả lại 1 tập đối tượng
+    public static ResultSet getDataFromQuery(String sql, Object... args) throws SQLException {
+        PreparedStatement pstm = getStmt(sql, args);
+        return pstm.executeQuery();
+    }
+
+    //4. Chuẩn bị câu truy vấn trước khi thực thi - Các varargs sử dụng dấu ba chấm (...) sau kiểu dữ liệu.
+    public static PreparedStatement getStmt(String sql, Object... args) {
+        try {
+            conn = getConnection();
+            PreparedStatement ps;
+            //ps = conn.prepareCall(sql) Gọi store procedure
+            System.out.println(sql + " " + args[0] + " " + args[1]);
+            ps = conn.prepareStatement(sql);//Dùng để triển khai các câu lệnh truy vấn thường
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);//Cộng các value sau câu truy vấn
+            }
+            return ps;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 }
