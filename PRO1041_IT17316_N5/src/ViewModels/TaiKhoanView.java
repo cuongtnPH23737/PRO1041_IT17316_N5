b49bd5f0dcafb0bd37e835f97a9ae7e15cc1898a
hoagduc27 <85150430+hoagduc27@users.noreply.github.com>
2022-11-16 21:50:54 +0700

    dangNhap_dangKy
    dangNhap_dangKy

---
+++ PRO1041_IT17316_N5/src/ViewModels/TaiKhoanView.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ViewModels;
+
+/**
+ *
+ * @author TRUNG DUC
+ */
+public class TaiKhoanView {
+     public String maTk,email,password,loaiTk;
+
+    public TaiKhoanView() {
+    }
+
+    public TaiKhoanView(String maTk, String email, String password, String loaiTk) {
+        this.maTk = maTk;
+        this.email = email;
+        this.password = password;
+        this.loaiTk = loaiTk;
+    }
+
+    public String getMaTk() {
+        return maTk;
+    }
+
+    public void setMaTk(String maTk) {
+        this.maTk = maTk;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoaiTk() {
+        return loaiTk;
+    }
+
+    public void setLoaiTk(String loaiTk) {
+        this.loaiTk = loaiTk;
+    }
+
+    @Override
+    public String toString() {
+        return "TaiKhoan{" + "maTk=" + maTk + ", email=" + email + ", password=" + password + ", loaiTk=" + loaiTk + '}';
+    }
+}
